8cda99c072320eea59b89a7f9420e871b52a339b
Noraluk <noraluk.kn@gmail.com>
2020-04-02 01:29:22 +0700

    fix : declare port

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,17 +1,17 @@
 package routes
 
 import (
+	"os"
 	"scg/api/controllers/doscg"
 
 	"github.com/labstack/echo"
 )
 
 func Run() {
-	var port string = "8080"
 	e := echo.New()
 
 	e.GET("/find-xyz", doscg.FindXYZ)
 	e.GET("/find-bc", doscg.FindBC)
 	e.POST("/callback", doscg.ReceiveLineMessage)
-	e.Logger.Fatal(e.Start(":" + port))
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
